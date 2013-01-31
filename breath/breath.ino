c7dd4cf75c3d327700d4739c1c6a02686351380a
George Redinger <george@georgeredinger.com>
2013-01-30 16:59:43 -0800

    filtered kst plot,misplaed paren in steinhart-hart equation in arduino

--- breath/breath.ino
+++ breath/breath.ino
@@ -13,7 +13,7 @@ void setup() {
   float vl,vr;
   float tr,tl;
   float rawl,rawr;
-  
+  float xvx;
   #define A 8.271111e-4
   #define B 2.088020e-4
   #define C 8.059200e-8
@@ -23,14 +23,16 @@ void setup() {
   void loop() { 
     rawl = analogRead(0);
     vl =  (rawl/4096)*3.3;
-    rleft = ((vl/3.3)/(1-vl/3.3))*249e3;
-    tl = (1/(A+B*(log(rleft)+C*pow(log(rleft),3)))-273.15)*1.8+32.0;
+    xvx = vl/3.3; 
+    rleft = ((xvx)/(1-xvx))*249e3;
+    tl = (1/(A+B*log(rleft)+C*pow(log(rleft),3))-273.15)*1.8+32.0;
     rawr = analogRead(1);
     vr = (rawr/4096)*3.3;
-    rright = ((vr/3.3)/(1-vr/3.3))*249e3;
-    tr = (1/(A+B*(log(rright)+C*pow(log(rright),3)))-273.15)*1.8+32.0;
+    xvx = vr/3.3; 
+    rright = ((xvx)/(1-xvx))*249e3;
+    tr = (1/(A+B*log(rright)+C*pow(log(rright),3))-273.15)*1.8+32.0;
     avg = (tl+tr)/2.0;
-    diff = tr-tl;
+    diff = tl-tr;
     Serial.print(avg);
     Serial.print(" ");
     Serial.print(diff);
